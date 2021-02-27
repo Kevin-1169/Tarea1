a29b37019d54f6955f526a6993c1fc4bc48dd547
Kevin-1169 <79121255+Kevin-1169@users.noreply.github.com>
2021-02-27 12:29:53 -0600

    Add files via upload

---
+++ Tarea 2 parte 2/Ej 1 pag 110/Ej 1 pag 110/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Ej_1_pag_110
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numero;
+
+            Console.WriteLine("ingrese un numero: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+          if (numero % 2 == 0)
+            {
+
+                Console.WriteLine("el numero es par ");
+
+            }
+          else
+            {
+                Console.WriteLine("el numero es impar ");
+            }
+
+            Console.ReadKey();
+          
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Tarea 2 parte 2/Ej 2 pag 110/Ej 2 pag 110/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Ej_2_pag_110
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radianes, grados, opcion;
+
+            Console.WriteLine("opcion 1 radianes a grados \nopcion 2 grados a radianes");
+            opcion = Convert.ToInt32(Console.ReadLine());
+
+            if(opcion==1)
+            {
+                Console.WriteLine("Ingrese los radianes:");
+                radianes = Convert.ToInt32(Console.ReadLine());
+                grados = radianes * 180 / 3.1418;
+
+                Console.WriteLine("Los grados son: " + grados);
+
+            }else if(opcion==2)
+            {
+                Console.WriteLine("Ingrese los grados: ");
+                grados = Convert.ToInt32(Console.ReadLine());
+                radianes = grados * 3.1418 / 180;
+
+                Console.WriteLine("Los radianes son: " + radianes);
+
+            }else
+            {
+                Console.WriteLine("No ha ingresado una opcion valida");
+
+            }
+
+
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
